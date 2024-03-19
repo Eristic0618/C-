028855fd8a7e160c5b4c1eec4a7340846b6638fb
Eristic <2779752984@qq.com>
2024-03-19 10:04:29 +0800

    实验2.1

---
+++ 实验2.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf(" Hello C-world!\n"); // 请注意，H字符的左边有一个空格。
+
+    printf(" ****\n"); // 请在此填写多行代码
+    printf(" *\n");
+    printf(" *\n");
+    printf(" ****\n");
+    return 0;
+}
